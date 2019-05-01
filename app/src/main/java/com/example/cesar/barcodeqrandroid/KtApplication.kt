d9bc285c9416a24cd0557062867b9463bfa991fb
cesar <cesarleandromayorga7@gmail.com>
2019-05-01 15:01:56 -0500

    commit one
    commit one

---
+++ app/src/main/java/com/example/cesar/barcodeqrandroid/KtApplication.kt
@@ -0,0 +1,10 @@
+package com.example.cesar.barcodeqrandroid
+
+import android.support.multidex.MultiDexApplication
+
+class KtApplication: MultiDexApplication(){
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
